e3ad40536c956a80bc73352d9c68b28789f9416e
Savannah Fox <savannahfox@gmail.com>
2019-03-13 10:51:35 -0700

    Initial commit of FizzBuzz project.

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+for (var i = 1; i < 101; i++) {
+    if(i % 15 == 0){
+        document.write("FizzBuzz!<br />");
+    }
+    else if(i % 3 == 0){
+        document.write("Fizz!<br />");
+    }
+    else if(i % 5 == 0){
+        document.write("Buzz!<br />");
+    }
+    else{
+        document.write(i);
+        document.write("<br />")
+    }
+}
